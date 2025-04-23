5ddb2806c1f8404f02049b6309c351e6c55de6a6
chaitanya405 <chaitanya405@gmail.com>
2025-04-23 13:02:38 -0400

    Update LRI_Code.py

--- LRI_Code.py
+++ LRI_Code.py
@@ -1,6 +1,6 @@
 import mysql.connector
 import logging
-class Node :
+class Node:
     
     def __init__(self,key,value) :
         self.key = key
@@ -8,57 +8,73 @@ class Node :
         self.prev = None
         self.next = None
 
-class mysqlDb :
+class mysqlDb:
     
-    def __init__(self,host,user,password,database) :
-        self.host = host
-        self.user = user
-        self.password = password
-        self.database = database
+    def __init__(self,config) :
+        self.host = config["host"]
+        self.user = config["user"]
+        self.password = config["password"]
     
-    def connect(self,config) :
+    def connect(self) :
         try :
-            self.conn = mysql.connector.connect(host=self.host,user=self.user,password=self.password,database=self.database)
+            self.conn = mysql.connector.connect(host=self.host,user=self.user,password=self.password)
             self.cursor = self.conn.cursor()
+            self.cursor.execute("create database IF NOT EXISTS mysql")
+            self.cursor.execute("create table IF NOT EXISTS mysql.mytable ( id varchar(10), state varchar(50));")
             logging.info("Connected to Database")
             return True
         except mysql.connector.Error as err :
             logging.error("Error in connecting to database")
             raise
                 
-    def getDataFromDb(self,key,config) : 
+    def getDataFromDb(self,key) : 
         try :
-            sql_query = f"select* from database.table where id = {key}"                
-            self.cursor.execute(sql)
+            sql_query = f"select state from mysql.mytable where id = {key}" 
+            self.connect()              
+            self.cursor.execute(sql_query)
             output=self.cursor.fetchall()
             logging.info("Fetched the Data from database") 
-            self.dbClose()            
-            return output       
+            self.cursor.close()            
+            return output[0]       
         except mysql.connector.Error as err :
             logging.error("Error in connecting to database")
             raise  
-    
-    def delDataFromDb(self,key,config) :
+    def getAllDataFromDb(self) : 
+        try :
+            sql_query = f"select * from mysql.mytable" 
+            self.connect() 
+            self.cursor.execute(sql_query)
+            output=self.cursor.fetchall()
+            logging.info("Fetched the Data from database") 
+            self.cursor.close()            
+            print(output)
+        except mysql.connector.Error as err :
+            logging.error("Error in connecting to database")
+            raise  
+
+    def delDataFromDb(self,key) :
         try :           
-            del_query = f"DELETE from database.table where id = {key}"
-            self.cursor.execute(del_sql)
+            del_query = f"DELETE from mysql.mytable where id = {key}"
+            self.connect() 
+            self.cursor.execute(del_query)
             self.conn.commit()
             logging.info("Deleted the Data from database")  
-            self.dbClose()            
+            self.cursor.close() 
+            logging.info("Database connection closed")           
         except mysql.connector.Error as err :
             logging.error("Error in deleting from database")
             raise
         
     
-    def dbClose() :    
-        self.mycusror.close()
-        self.db.close()
-        logging.info("Database connection closed")
+    #def dbClose(self,delcursor) :    
+    #    delcursor.close()
+        #self.db.close()
+        #logging.info("Database connection closed")
         
 
-class testCache :
+class testCache:
     
-    def _init_(self,max_elements) :
+    def __init__(self,max_elements) :
         self.max_elements = max_elements
         self.cache = {}
         
@@ -91,15 +107,14 @@ class testCache :
                self.add_to_the_top(node)
                return node.value
             else :
-               db = mysqlDb(db_config)               
+               db = mysqlDb(db_config)            
                result = db.getDataFromDb(key)
                if result :
                    value = result[0]
                    self.putCache(key,value)
-                   db.dbClose()
                    return value
                else :
-                   logging.info("Cannot Retrieve the data from Ccahe or Database")
+                   logging.info("Cannot Retrieve the data from Cache or Database")
                    
         except Exception as e :
                logging.error("Cannot Retrieve the data")
@@ -130,14 +145,13 @@ class testCache :
                 logging.info("Deleting data from database")                
                 db = mysqlDb(db_config)                
                 db.delDataFromDb(key)
-                db.dbClose()
         except Exception as e :
                logging.error("Cannot Delete the data")
                raise        
 
-    def removeAllData(self,cache_data,db_config) :
+    def removeAllData(self,db_config) :
         try :
-            for key in cache_data:
+            for key in list(self.cache):
                 self.removeData(key,db_config)
         except Exception as e :
                logging.error("Cannot Delete the Cache")
@@ -145,12 +159,17 @@ class testCache :
     def clearCache(self,cache_data) :
         cache_data.clear() 
     def getCacheData(self) :
-        return self.cache        
-         
-                   
+        cache_data = {}
+        for key in list(self.cache) :
+            node = self.cache[key]
+            cache_data[key] = node.value
+        print(cache_data)    
+        
+              
+                            
 
-if __name__ == "main" :
-    db_config = { "host" : "host" , "user" : "user", "password" : "password", "database" : "database"}
+#if __name__ == "main" :
+db_config = { "host" : "localhost" , "user" : "myuser", "password" : "password"}
     
     
     # connecting to database 
@@ -159,46 +178,59 @@ if __name__ == "main" :
     
     #  Initializing Cache Capacity to 3 elements 
     
-    tempCache = testCache(3)
-    
+tempCache = testCache(3)
+
+dbData = mysqlDb(db_config)
+# Current data from the database 
+dbData.getAllDataFromDb()    
     # Adding data to Cache
-    tempCache.putCache('1' , 'New Jersey')
-    tempCache.putCache('2' , 'Delaware')
-    tempCache.putCache('3' , 'New York')
+tempCache.getCache('1',db_config)
+tempCache.getCache('2',db_config)
+tempCache.getCache('3',db_config)
+tempCache.getCacheData()
+
+# Current data from the database 
+dbData.getAllDataFromDb() 
     
     # Getting the elemnt values from cache
-    print(tempCache.getCache('1',db_config))
-    print(tempCache.getCache('2',db_config))
+print(tempCache.getCache('1',db_config))
+print(tempCache.getCache('2',db_config))
+
+print(tempCache.getCache('6',db_config))
     
     #Adding extra element more than capacity 
-    tempCache.putCache('4' , 'California')
+tempCache.getCache('4' , db_config)
     
     # Checking the cache to see the updated elements after deleting the least used one
-    
-    print(tempCache.getCacheData())
+tempCache.getCacheData()
+
+# Current data from the database 
+dbData.getAllDataFromDb() 
     
     # Removing the data from cache
-    tempCache.removeData('4',db_config)
+tempCache.removeData('1',db_config)
     
-    print(tempCache.getCacheData())
+tempCache.getCacheData()
+# Current data from the database 
+dbData.getAllDataFromDb() 
     
     # Removing all the data from cache and Database
-    tempCache.removeAllData(cache,db_config)
-    print(tempCache.getCacheData())
+tempCache.removeAllData(db_config)
+
+tempCache.getCacheData()
+# Current data from the database 
+dbData.getAllDataFromDb() 
     
     # Adding the element to cache
     
-    tempCache.putCache('5' , 'Florida')
+tempCache.getCache('5' , db_config)
+tempCache.getCacheData()
     
     # Clearing the Cache
-    tempCache.clearCache(cache)  
+tempCache.clearCache(tempCache.cache)  
     
-    print(tempCache.getCacheData())    
-    
-    
-    
-    
-    
-    
-    
-    
+tempCache.getCacheData()  
+
+# Current data from the database 
+dbData.getAllDataFromDb() 
+
